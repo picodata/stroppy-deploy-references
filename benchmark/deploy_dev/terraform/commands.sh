9e70db839f01af6b00a5efb61d1f57b451158e06
Bakhtiarov Sergey <bakhtiarov.sergey@gmail.com>
2021-06-16 13:51:30 +0300

    fix: monitoring for yandex.cloud updated

--- benchmark/deploy_dev/terraform/commands.sh
+++ benchmark/deploy_dev/terraform/commands.sh
@@ -85,27 +85,20 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 chmod 600 $HOME/.kube/config
-# monitoring
-kubectl create namespace monitoring
-# helm repo add grafana https://grafana.github.io/helm-charts
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo update
-# helm install loki grafana/loki-stack --namespace monitoring
-# helm install grafana-stack prometheus-community/kube-prometheus-stack --namespace monitoring
-kubectl apply -f /home/ubuntu/metrics-server.yaml
-# kubectl apply -f /home/ubuntu/ingress-grafana.yaml
+# local-storage
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
-# grafana-on-premise
-ansible-galaxy install cloudalchemy.prometheus
+# monitoring: kube-prometheus-stack
+kubectl create namespace monitoring
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install grafana-stack prometheus-community/kube-prometheus-stack --set grafana.enables=false --namespace monitoring
+# monitoring: grafana-on-premise
 ansible-galaxy install cloudalchemy.grafana
-ansible-galaxy install cloudalchemy.node_exporter
 ansible-galaxy collection install community.grafana
 cd ../grafana-on-premise
+export prometheus_cluster_ip=$(kubectl get svc -n monitoring | grep grafana-stack-kube-prometh-prometheus | awk "{ print$ 3 }")
+sed -i "s/      ds_url: http:\/\/localhost:9090/      ds_url: http:\/\/$prometheus_cluster_ip:9090/g" grafana-on-premise.yml
 ansible-playbook grafana-on-premise.yml
-ansible-playbook node_exporter.yml
-echo '  - worker-1:9100' | sudo tee -a /etc/prometheus/file_sd/node.yml
-echo '  - worker-2:9100' | sudo tee -a /etc/prometheus/file_sd/node.yml
-echo '  - worker-3:9100' | sudo tee -a /etc/prometheus/file_sd/node.yml
 EOO
 chmod +x deploy_kubernetes.sh
 ./deploy_kubernetes.sh
